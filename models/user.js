09ace414fd4c493b10c5ee7f5f25c8a9c4669d2d
talexcrowell <talexcrowell@gmail.com>
2018-10-29 12:39:44 -0700

    updated usersRouter and usersSchema with bcryptjs

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   fullname: String,
@@ -18,8 +19,11 @@ userSchema.set('toJSON', {
 });
 
 userSchema.methods.validatePassword = function(password) {
-  return password === this.password;
+  return bcrypt.compare(password, this.password);
 };
 
+userSchema.statics.hashPassword = function(password){
+  return bcrypt.hash(password, 10);
+};
 
 module.exports = mongoose.model('User', userSchema);
--- routes/users.js
+++ routes/users.js
@@ -11,32 +11,29 @@ const router = express.Router();
 router.post('/', (req, res, next) => {
   const { username, fullname, password } = req.body;
 
-  const newUser = { username, fullname, password };
-
   /***** Never trust users - validate input *****/
   return User.find({ username })
-    .count()
-    .then(count => {
-      if(count >0) {
-        return Promise.reject({
-          code: 422,
-          reason:'ValidationError',
-          message: 'Username already taken',
-          location: 'username'
+    .then(()=> {
+      return User.hashPassword(password)
+        .then(digest => {
+          const newUser = {
+            username,
+            fullname,
+            password: digest
+          };
+          return User.create(newUser);
+        })
+        .then(result => {
+          return res.location(`/api/user/${result.id}`).status(201).json(result);
+        })
+        .catch(err => {
+          if (err.code === 11000) {
+            err = new Error('The username already exists');
+            err.status = 400;
+          }
+          next(err);
         });
-      }
-      return User.create(newUser);
-    })
-    .then(user => {
-      return res.location(`/api/user/${user.id}`).status(201).json(user);
-    })
-    .catch(err => {
-      if (err.reason === 'ValidationError'){
-        return res.status(err.code).json(err);
-      }
-      res.status(500).json({ code: 500, message: 'Internal server error' });
     });
-
 });
 
 module.exports = router;
