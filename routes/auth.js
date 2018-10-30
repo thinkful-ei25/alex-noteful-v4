95854b5c8a92526fe8e1ff30a5d0458d18a8b845
talexcrowell <talexcrowell@gmail.com>
2018-10-30 11:44:04 -0700

    updated auth endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -2,15 +2,26 @@
 
 const express = require('express');
 const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+const { JWT_SECRET, JWT_EXPIRY } = config; 
 
 const router = express.Router();
 
 const options = {session: false, failWithError: true};
 
+function createAuthToken (user) {
+  return jwt.sign({ user }, JWT_SECRET, {
+    subject: user.username,
+    expiresIn: JWT_EXPIRY
+  });
+}
+
 const localAuth = passport.authenticate('local', options);
 
 router.post('/', localAuth, function(req, res){
-  return res.json(req.user);
+  const token = createAuthToken(req.user.toJSON());
+  return res.json({ token });
 });
 
 module.exports = router;
